626010b208dd74c283552d9a8c774e6abf8fd29f
williamsa <williams.adeho@cdl.co.uk>
2015-07-03 16:20:14 +0100

    Initial Implementation

---
+++ ERoomApp/src/main/java/eroom/ERoomAppApplication.java
@@ -0,0 +1,12 @@
+package eroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERoomAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERoomAppApplication.class, args);
+    }
+}
