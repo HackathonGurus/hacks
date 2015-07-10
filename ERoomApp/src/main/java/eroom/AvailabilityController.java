dbce25fbb63a8cfcb7d6792cd5c4730be4c99b6a
Timothy Pharo <timothy.pharo@cdl.co.uk>
2015-07-10 15:10:53 +0100

    Added GoHome button to availability page

---
+++ ERoomApp/src/main/java/eroom/AvailabilityController.java
@@ -0,0 +1,23 @@
+package eroom;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AvailabilityController {
+
+	@RequestMapping(Links.AVAILABILITY)
+	public String availabilityHome(){
+
+		return "availability";
+	}
+	
+	//Send them on their way
+	@RequestMapping(value="/goHome", method = RequestMethod.POST)
+	public String goHome(){
+		return "redirect:/";
+	}
+	
+}
+
