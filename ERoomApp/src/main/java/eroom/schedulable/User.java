2ca64292dda8fbbafa546650901835314f89d1f0
Thrand <jordan@lyndons.net>
2015-07-10 17:35:25 +0100

    added user to fix

--- ERoomApp/src/main/java/eroom/schedulable/User.java
+++ ERoomApp/src/main/java/eroom/schedulable/User.java
@@ -9,6 +9,8 @@ public class User extends ScheduleObject {
 
     /** The name of the user */
     private String name;
+    
+    private String email;
 
     public User(String name) {
         super();
@@ -18,5 +20,13 @@ public class User extends ScheduleObject {
     public String getName() {
         return this.name;
     }
+
+	public String getEmailAddress() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
