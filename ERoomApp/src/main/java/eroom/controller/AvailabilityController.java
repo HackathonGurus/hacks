0c02e481352fab9f22de7a60be331b863fdf3fd1
Timothy Pharo <timothy.pharo@cdl.co.uk>
2015-07-10 16:00:08 +0100

    Added table that loops through available rooms. Currently not wired up

--- ERoomApp/src/main/java/eroom/controller/AvailabilityController.java
+++ ERoomApp/src/main/java/eroom/controller/AvailabilityController.java
@@ -1,23 +1,32 @@
 package eroom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import eroom.Links;
+import eroom.calendar.Calendar;
 
 @Controller
 public class AvailabilityController {
 
 	
 	@RequestMapping(Links.AVAILABILITY)
-	public String availabilityHome(){
+	public String availabilityHome(Model model){
+		//Should maybe be the Cal from the session or a shared instance
+		//Calendar cal = new Calendar();
 		
-		
-		
-		
+		//model.addAttribute("rooms", cal.getRooms());
 		
 		return "availability";
 	}
 	
+	
+	//Send them on their way
+	@RequestMapping(value="/goHome", method = RequestMethod.POST)
+	public String goHome(){
+		return "redirect:/";
+	}
 	
 }
