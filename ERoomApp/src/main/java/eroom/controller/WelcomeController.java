c171860fe4ed6f121e1bcd758dbcc12e4d5e2913
Ryan <ryanc@LT-11131.cheshdatasys.co.uk>
2015-07-10 18:24:15 +0100

    Tweaks to welcome page and controller

--- ERoomApp/src/main/java/eroom/controller/WelcomeController.java
+++ ERoomApp/src/main/java/eroom/controller/WelcomeController.java
@@ -1,16 +1,24 @@
 package eroom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import eroom.Links;
+import eroom.calendar.Appointment;
 
 @Controller
 public class WelcomeController {
 	
 	@RequestMapping(Links.WELCOME)
-	public String welcomeHome()  {		
+	public String welcomeHome(Model model)  {		
+		
+		//Test latest appointment
+		Appointment apt = new Appointment().withRoom("Room 1").withDescription("Desc 1").withOrganiser("Person 1");
+		
+		model.addAttribute("latestApt", apt);
+		
 		return "welcome";
 	}
-	
+		
 }
