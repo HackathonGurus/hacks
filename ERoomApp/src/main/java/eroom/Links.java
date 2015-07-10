2ef4a1a88882b3b8347cf652956e442e0f48aa40
williamsa <williams.adeho@cdl.co.uk>
2015-07-10 18:21:52 +0100

    Added info button to booking details page

--- ERoomApp/src/main/java/eroom/Links.java
+++ ERoomApp/src/main/java/eroom/Links.java
@@ -9,19 +9,17 @@ public final class Links {
 	public static final String AVAILABILITY = "/avail";
 	public static final String WELCOME = "/welcome";
 	public static final String QUICK_LINK_TEST = "/test";
-	public static final String BOOKING_DETAILS = "/bookingdetails";
+	public static final String BOOKING_DETAILS = "/bookingDetails";
 	
 	private static List<String> links  = new ArrayList<String>();
-	
-	
+		
 	static {
 		links.add(LOGIN);
 		links.add(AVAILABILITY);
 		links.add(QUICK_LINK_TEST);
 		links.add(WELCOME);
 		links.add(BOOKING_DETAILS);
-	}
-	
+	}	
 	
 	public static List<String> getLinks() {
 		return links;
