f7eeac55843fc970c45d2f7e9b0021141e0708b6
williamsa <williams.adeho@cdl.co.uk>
2015-07-13 07:21:35 +0100

    Finalised Styling and sorted out booking details page

--- ERoomApp/src/main/resources/static/scripts/dateTime.js
+++ ERoomApp/src/main/resources/static/scripts/dateTime.js
@@ -4,6 +4,7 @@
 var currentTime = document.getElementById("currentTime");
 var currentDate = document.getElementById("currentDate");
 updateTime();
+updateDate();
 
 // Set the updateTime method to trigger every second
 setInterval(updateTime, 1000);
@@ -13,12 +14,36 @@ function updateTime() {
 	var dt = new Date();
 	var hours = dt.getHours();
 	var minutes = dt.getMinutes();
+	var seconds = dt.getSeconds();
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	currentTime.innerHTML = hours + ":" + minutes;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	currentTime.innerHTML = hours + ":" + minutes + ":" + seconds;
 }
+
+function updateDate() {
+	var today = new Date();
+	var montharray=new Array("January","February","March","April","May","June",
+            "July","August","September","October","November","December")
+	var strDate = 'd-m-Y'
+	  .replace('Y', today.getFullYear())
+	  .replace('m', montharray[today.getMonth()])
+	  .replace('d', today.getDate());
+	
+	
+	currentDate.innerHTML = strDate;
+}
+
+$(document).ready(function() {  
+	 setInterval(function() {
+	    $('.box').animate( { color: '#40e0d0' }, 300)
+	    .animate( { color: '#049cdb' }, 300); 
+	    }, 1000);
+	 });
 
